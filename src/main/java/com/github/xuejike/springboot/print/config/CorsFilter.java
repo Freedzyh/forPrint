9122011b0084f20e1229f93454d8a00f3a54407a
王露 <1186956992@qq.com>
2018-03-26 15:59:20 +0800

    提交

--- src/main/java/com/github/xuejike/springboot/print/config/CorsFilter.java
+++ src/main/java/com/github/xuejike/springboot/print/config/CorsFilter.java
@@ -19,18 +19,26 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Access-Control-Allow-Origin: 指定授权访问的域
-        response.addHeader("Access-Control-Allow-Credentials", "true");  //此优先级高于@CrossOrigin配置
+//        response.addHeader("Access-Control-Allow-Credentials", "true");  //此优先级高于@CrossOrigin配置
         response.addHeader("Access-Control-Allow-Origin", "*");  //此优先级高于@CrossOrigin配置
+
+
+        response.addHeader("Access-Control-Allow-Headers", "Origin, x-requested-with, Content-Type, Accept,X-Cookie");
+
 
         // Access-Control-Allow-Methods: 授权请求的方法（GET, POST, PUT, DELETE，OPTIONS等)
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE，OPTIONS");
 
-//        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-
-//        response.addHeader("Access-Control-Allow-Headers", "X-Requested-With，X- HTTP-Method-Override，Content-Type，Accept");
-        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
         response.addHeader("Access-Control-Max-Age", "1800");//30 min
+
+
+        if (request.getMethod().equals("OPTIONS") ) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+
         filterChain.doFilter(request, response);
     }
 }
